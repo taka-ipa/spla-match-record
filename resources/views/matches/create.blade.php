921a2c64c3aae6203fb546cc2792bf6f7090ade7
taka-ipa <taka.ipa1110@gmail.com>
2025-03-30 22:50:08 +0900

    form create

--- resources/views/matches/create.blade.php
+++ resources/views/matches/create.blade.php
@@ -1,27 +1,38 @@
 <x-app-layout>
-    test
-<form action="{{ route('matches.create') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <h3 class="text-lg font-semibold mb-2">ステージを選択</h3>
+    <form action="{{ route('matches.create') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <select name="stage" class="block w-1/3 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none">
+            <option value="">ステージを選択</option>
+            @foreach ($stages as $stage)
+                <option value="{{ $stage->id }}">{{ $stage->name }}</option>
+            @endforeach
+        </select><br>
 
-    <select name="stage" class="block w-full px-4 py-2 border border-gray-300 rounded-lg 
-        focus:ring-2 focus:ring-blue-400 focus:outline-none">
-        <option value="">ステージを選択</option>
-        @foreach ($stages as $stage)
-            <option value="{{ $stage->id }}">{{ $stage->name }}</option>
-        @endforeach
-    </select>
-    <label>ルール: <input type="text" name="rule"></label><br>
-    <label>武器: <input type="text" name="weapon"></label><br>
-    <label>勝敗:
-        <select name="result">
+        <select name="rule" class="block w-1/3 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none">
+            <option value="">ルールを選択</option>
+            @foreach ($rules as $rule)
+                <option value="{{ $rule->id }}">{{ $rule->name }}</option>
+            @endforeach
+        </select><br>
+
+        <select name="weapon" class="block w-1/3 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none">
+            <option value="">武器を選択</option>
+            @foreach ($weapons as $weapon)
+                <option value="{{ $weapon->id }}">{{ $weapon->name }}</option>
+            @endforeach
+        </select><br>
+
+        <select name="result" class="block w-1/3 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none">
             <option value="win">勝ち</option>
             <option value="lose">負け</option>
-        </select>
-    </label><br>
-    <label>コメント: <textarea name="comment"></textarea></label><br>
-    <button type="submit" class="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
-        送信
-    </button>
-</form>
+        </select><br>
+
+        <label>バトルメモリー: <input type="text" name="replay_code" class="block w-1/3 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"></label><br>
+
+        <label>コメント: <textarea name="comment" class="block w-1/3 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"></textarea></label><br>
+
+        <button type="submit" class="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
+            送信
+        </button>
+    </form>
 </x-app-layout>
